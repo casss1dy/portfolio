1a9c654e5fd9dc52873df6b86b605422f086f346
Nadya Khramova <casssidy@ya.ru>
2019-12-01 01:01:25 +0400

    feat: add repair project

--- assets/js/frame.js
+++ assets/js/frame.js
@@ -2,6 +2,9 @@ const body = document.querySelector('body');
 const frame = document.querySelector('.frame');
 
 document.querySelector('.btn-mobile').addEventListener('click', () => {
+  let scrollTop = window.pageYOffset;
+  console.log(scrollTop);
   frame.classList.toggle('mobile');
   body.classList.toggle('backdrop');
+  window.scrollTo(0, scrollTop + 'px');
 });
